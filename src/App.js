33eb33d376a010049b14fe8d2e7c9a3988d8efa3
DomeAustinB <100167612+DomeAustinB@users.noreply.github.com>
2022-10-20 18:54:22 -0400

    under construction banner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/images/construction.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+        World Business Lenders Youth Foundation 
+        </h1>
+        <p2>
+          Please Try Again Later
+        </p2>
+        
       </header>
     </div>
   );
